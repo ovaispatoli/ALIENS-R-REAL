451f42668c1159c238f0ffadfae3a4684356fba5
OvaisPatoli <ovaispatoli@gmail.com>
2019-05-18 15:57:35 -0500

    created function of data population.

--- Static_Code/JS/app.js
+++ Static_Code/JS/app.js
@@ -1,15 +1,31 @@
-// Table Data Variable
+//Variable Objects
 var tableData = data;
-
-// Table body variable
+var button = d3.select("filter-btn");
+var field = d3.select("#datetime");
 var tbody = d3.select("tbody");
 
-// console.log(data);
 
-data.forEach(function(mydata) {
-    // console.log(mydata);
-    var row = tbody.append("tr");
-    Object.values(mydata).forEach((value) => {
-        row.append("td").text(value);
+
+// Populate data
+
+// data.forEach(function(mydata) {
+//     // console.log(mydata);
+//     var row = tbody.append("tr");
+//     Object.values(mydata).forEach((value) => {
+//         row.append("td").text(value);
+//     });
+// });
+
+
+
+var populate = (someData) => {
+    someData.forEach(function(mydata) {
+        // console.log(mydata);
+        var row = tbody.append("tr");
+        Object.values(mydata).forEach((value) => {
+            row.append("td").text(value);
+        });
     });
-});
+}
+
+populate(data);
