3f5a5efdc8cbe46467382b3ce107ecf1da5e40f3
OvaisPatoli <ovaispatoli@gmail.com>
2019-05-18 17:08:28 -0500

    finished filtration code.

--- Static_Code/JS/app.js
+++ Static_Code/JS/app.js
@@ -1,31 +1,59 @@
 //Variable Objects
 var tableData = data;
-var button = d3.select("filter-btn");
-var field = d3.select("#datetime");
+var filterButton = d3.select("#filter-btn");
+var dateField = d3.select("#datetime");
 var tbody = d3.select("tbody");
-
-
 
 // Populate data
 
-// data.forEach(function(mydata) {
-//     // console.log(mydata);
-//     var row = tbody.append("tr");
-//     Object.values(mydata).forEach((value) => {
-//         row.append("td").text(value);
-//     });
-// });
+/*tableData.forEach(function(mydata) {
+    // console.log(mydata);
+    var row = tbody.append("tr");
+    Object.values(mydata).forEach((value) => {
+        row.append("td").text(value);
+    });
+});
+*/
 
-
-
-var populate = (someData) => {
+var populateData = (someData) => {
     someData.forEach(function(mydata) {
         // console.log(mydata);
-        var row = tbody.append("tr");
+        let row = tbody.append("tr");
         Object.values(mydata).forEach((value) => {
             row.append("td").text(value);
         });
     });
 }
 
-populate(data);
+populateData(tableData);
+
+// Filter 
+
+filterButton.on("click", function() {
+    
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select input element from index.html
+    let inputValue = dateField.property("value");
+    console.log(inputValue);
+    
+    // Filter table data by field matching input value
+    let filteredData = tableData.filter(myData => myData.datetime === inputValue);
+    console.log(filteredData);
+
+    //Populate Data based on the input value
+    tbody.html("");
+    
+    let response = filteredData;
+
+    if (response.length !== 0) {
+        populateData(filteredData);
+    }
+        else {
+            tbody.append("tr").append("td").text("Your input didn't match any of our data. Try again.");
+        }
+
+
+})
+
