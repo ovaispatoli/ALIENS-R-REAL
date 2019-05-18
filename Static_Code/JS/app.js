11235f9ec936f8184c2b1035514194d1b6530169
OvaisPatoli <ovaispatoli@gmail.com>
2019-05-18 15:23:25 -0500

    Was able to populate data into html

--- Static_Code/JS/app.js
+++ Static_Code/JS/app.js
@@ -1,1 +1,15 @@
+// Table Data Variable
 var tableData = data;
+
+// Table body variable
+var tbody = d3.select("tbody");
+
+// console.log(data);
+
+data.forEach(function(mydata) {
+    // console.log(mydata);
+    var row = tbody.append("tr");
+    Object.values(mydata).forEach((value) => {
+        row.append("td").text(value);
+    });
+});
